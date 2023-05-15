d1847831317816700922344e13cacd425d9e4225
sam <sam821203@yahoo.com.tw>
2023-05-15 16:54:13 +0800

    refactor: replace remove setInterval function with animationend event

--- following-touch-firecracker/script.js
+++ following-touch-firecracker/script.js
@@ -2,23 +2,28 @@ const touchWrap = document.querySelector(".touch__wrap");
 
 function buildTouchAnimate() {
   // Mobile
-  document.addEventListener("touchstart", addWillChange);
-  document.addEventListener("touchstart", firecrackerTouchStart);
-  document.addEventListener("touchmove", addWillChange);
-  document.addEventListener("touchmove", firecrackerTouchMove);
-  document.addEventListener("touchend", removeSparkle);
+  // document.addEventListener("touchstart", addWillChange);
+  // document.addEventListener("touchstart", firecrackerTouchStart);
+  // document.addEventListener("touchmove", addWillChange);
+  // document.addEventListener("touchmove", firecrackerTouchMove);
+  // document.addEventListener("touchend", removeSparkle);
 
   // Web
   document.addEventListener("click", firecrackerMouseClick);
   document.addEventListener("mousemove", addWillChange);
   document.addEventListener("mousemove", firecrackerMouseMove);
-  document.addEventListener("mousemove", firecrackerMouseMoveStop);
-
-  // 每 2 秒檢查一次
-  // var generateSparkle = setInterval(removeSparkle, 2000);
-  // clearInterval
-  // touchend 時，就把所有的點清空
 }
+
+// 產生範圍內的隨機數字
+function randomInRange(min, max) {
+  return Math.floor(Math.random() * (max - min) + 1) + min;
+}
+
+function updateFirecrackerPosition(el, identifier) {
+  el.style.top = `${identifier.clientY}px`;
+  el.style.left = `${identifier.clientX}px`;
+}
+
 // 預期 touchWrap 裡子層會發生改變
 function addWillChange() {
   const childArray = Array.from(touchWrap.childNodes);
@@ -29,50 +34,13 @@ function addWillChange() {
 }
 
 // 當 CSS 動畫結束後清除 will-change 屬性
-function removeWillChange() {
-  const childArray = Array.from(touchWrap.childNodes);
+// function removeWillChange() {
+//   const childArray = Array.from(touchWrap.childNodes);
 
-  childArray.forEach(function (el) {
-    el.classList.remove("js-willChange");
-  });
-}
-
-// 當滑鼠停止滑動時，清除掉 will-change
-function firecrackerMouseMoveStop() {
-  let timer;
-  let delayTime = 250;
-
-  clearTimeout(timer);
-  timer = setTimeout(removeSparkle(), delayTime);
-}
-
-// 產生範圍內的隨機數字
-function randomInRange(min, max) {
-  return Math.floor(Math.random() * (max - min) + 1) + min;
-}
-
-// 當 touchEnd 時，清除 touchWrap 裡的所有子元素
-function removeSparkle() {
-  const currentSparkleCount = touchWrap.children.length;
-  while (touchWrap.firstChild && currentSparkleCount >= 50) {
-    touchWrap.removeChild(touchWrap.firstChild);
-  }
-}
-
-function removeSparkleWithNumber() {
-  let maxLength = 60;
-  const childArray = Array.from(touchWrap.childNodes);
-
-  // 檢查 touchWrap 裡的 child nodes 數量
-  // 超過 maxLength，就刪除前面一半數量的元素
-  if (childArray.length > maxLength) {
-    let newChildArray = childArray.splice(0, maxLength / 2);
-
-    newChildArray.forEach(function (el) {
-      el.remove();
-    });
-  }
-}
+//   childArray.forEach(function (el) {
+//     el.classList.remove("js-willChange");
+//   });
+// }
 
 // 生成火花動畫
 function firecrackerAnimate(el, max, minTravel, maxTravel) {
@@ -99,45 +67,13 @@ function firecrackerAnimate(el, max, minTravel, maxTravel) {
   // 加上淡入淡出動畫
   el.style.animation = `fade-out ${animationDuration}s ease-out forwards`;
 
+  // 當該元素的動畫結束後，移除該元素
+  el.addEventListener('animationend', function() {
+    this.remove();
+  });
+
   // 新增至頁面上
   touchWrap.appendChild(el);
-}
-
-function updateFirecrackerPosition(el, identifier) {
-  el.style.top = `${identifier.clientY}px`;
-  el.style.left = `${identifier.clientX}px`;
-}
-
-// touch start
-function firecrackerTouchStart(touches) {
-  // 偵測不同手指
-  [...touches.changedTouches].forEach((touch) => {
-    const firecracker = document.createElement("div");
-
-    firecracker.classList.add("sparkler");
-
-    // 生成火花
-    firecrackerAnimate(firecracker, 40, 80, 140);
-
-    // 偵測並更新 X / Y 軸位置
-    updateFirecrackerPosition(firecracker, touch);
-  });
-}
-
-// touch move
-function firecrackerTouchMove(touches) {
-  // 偵測不同手指
-  [...touches.changedTouches].forEach((touch) => {
-    const firecracker = document.createElement("div");
-
-    firecracker.classList.add("sparkler");
-
-    // 生成火花
-    firecrackerAnimate(firecracker, 20, 50, 120);
-
-    // 偵測並更新 X / Y 軸位置
-    updateFirecrackerPosition(firecracker, touch);
-  });
 }
 
 // mouse click
@@ -169,5 +105,37 @@ function firecrackerMouseMove(mouse) {
   firecracker.style.top = `${firecracker.offsetTop - 10}px`;
   firecracker.style.opacity = 0;
 }
+
+// touch start
+// function firecrackerTouchStart(touches) {
+//   // 偵測不同手指
+//   [...touches.changedTouches].forEach((touch) => {
+//     const firecracker = document.createElement("div");
+
+//     firecracker.classList.add("sparkler");
+
+//     // 生成火花
+//     firecrackerAnimate(firecracker, 40, 80, 140);
+
+//     // 偵測並更新 X / Y 軸位置
+//     updateFirecrackerPosition(firecracker, touch);
+//   });
+// }
+
+// touch move
+// function firecrackerTouchMove(touches) {
+//   // 偵測不同手指
+//   [...touches.changedTouches].forEach((touch) => {
+//     const firecracker = document.createElement("div");
+
+//     firecracker.classList.add("sparkler");
+
+//     // 生成火花
+//     firecrackerAnimate(firecracker, 20, 50, 120);
+
+//     // 偵測並更新 X / Y 軸位置
+//     updateFirecrackerPosition(firecracker, touch);
+//   });
+// }
 
 document.addEventListener("DOMContentLoaded", buildTouchAnimate);
